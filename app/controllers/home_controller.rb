c35073fd882309baa7dc02bc02830a24537fd8f3
HiroyukiTamae <e165759@ie.u-ryukyu.ac.jp>
2018-10-16 02:08:19 +0900

    DB Connect Savings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
   def create
     @user = User.new(name:params[:name])
     @user.save
+    @saving = Saving.new(target: 0, current: 0, system_id: @user.id)
+    @saving.save
     redirect_to("/savings/main/#{@user.id}")
   end
 end
--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -1,5 +1,56 @@
 class SavingsController < ApplicationController
-  def main
-    @user = User.find(params[:id])
-  end
+
+    def main
+        # @savings = Saving.first
+        @user_id = params[:id]
+        @savings = Saving.find_by(system_id: params[:id])
+        @savings_current = @savings.current
+        @savings_target = @savings.target
+    end
+    
+    def new
+        @user = User.new
+        @savings = Saving.new
+    end
+    
+
+    def create
+        @savings = Saving.find_by(system_id: params[:id])
+        @savingsAdd = (@savings.current + params[:saving][:current].to_i)
+        
+        # 成功した時
+        # if @savings.save
+        if @savings.update(current: @savingsAdd)
+            redirect_to "/savings/main/#{params[:id]}", notice: "保存しました"
+        
+        # 失敗
+        else
+            render :new, notice: "保存できませんでした"
+        end
+        
+    end
+    
+    
+    def update
+        
+        @savings = Saving.find_by(system_id: params[:id])
+        @savingsNew = params[:saving][:target].to_i
+        @user_id = params[:id]
+        # 成功した時
+        # if @savings.save
+        if @savings.update(target: @savingsNew)
+            redirect_to "/savings/main/#{@user_id}", notice: "保存しました"
+        
+        # 失敗
+        else
+            render :new, notice: "保存できませんでした"
+        end
+    end
+    
+    private
+    
+    def savings_params
+        params.require(:saving).permit(:current, :system_id)
+    end
+    
 end
